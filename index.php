2f274ed99bef7069b37f4b275842578c8ace2045
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2021-09-16 19:11:51 +0700

    df

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $regis = curl("https://api.rekeningku.com/v2/price");
 $result = json_decode($regis, true);
 
 // fungsi penyederhanaan satuan rupiah
-function Simp($rp)
+function Simpl($rp)
 {
     $simp = ['', '', 'Ribu', 'Juta', 'Milyar', 'Trilyun', 'Kuadriliun'];
     $input = explode(".", $rp); // into index
@@ -33,17 +33,19 @@ function KIcon($icons)
     <header>
         <h2>Markets - Pull API Rekeningku.com v2</h2>
     </header>
-    <main class="container-fluid">
+    <main class="container-fluid ">
+        <!-- Btn -->
         <div class="containter bg-white py-2">
             <!-- <a href="exportData.php" target="_blank">Export Data</a> -->
             <a href="retrieveData.php" target="_blank" class="btn btn-sm btn-primary">Ambil Data</a>
             <a href="retrieveDataSingle.php" target="_blank" class="btn btn-sm btn-success">Ambil Data Another Method</a>
             <a href="perolehanData.php" target="_blank" class="btn btn-sm btn-warning">Lihat Perolehan Data</a>
         </div>
-        <div class="">
+        <!-- Table -->
+        <div class="bg-white">
             <table id="coins" class="table table-sm table-responsive table-hover shadow">
                 <thead class="bg-dark text-light fw-bold">
-                    <th scope="col" class="">Pair</th>
+                    <th scope="col" class="ps-2">Pair</th>
                     <th scope="col">Koin</th>
                     <th scope="col">Harga Terakhir</th>
                     <th scope="col">Perubahan 24 Jam</th>
@@ -55,14 +57,14 @@ function KIcon($icons)
                     <!-- <th scope="col">Last Transaction</th> -->
                     <!-- <th>Security Token</th> -->
                 </thead>
-                <tbody class="bg-white">
+                <tbody class="ps-2">
                     <?php $icons = [];
                     $i = 0;
                     foreach ($result as $tickers => $a) { ?>
                         <tr>
                             <?php $icons[$i] = $a['cd']; ?>
-                            <td class="ps-2">
-                                <div class="d-flex align-items-center py-2 pr-3 w-100">
+                            <td>
+                                <div class="d-flex align-items-center p-2 w-100">
                                     <img width="23" src="<?= KIcon($icons); ?>" alt="">
                                     <div class="d-flex align-items-end ms-2">
                                         <span class="fw-bold font-14px markets-primary-pair"><?= $a['cd']; ?></span>
@@ -92,7 +94,7 @@ function KIcon($icons)
                                 <div class="d-flex align-items-center py-2 pr-3 w-100"><?= number_format($a['v'], 2, ',', '.'); ?></div>
                             </td>
                             <td>
-                                <div class="d-flex align-items-center py-2 pr-3 w-100"><?= Simp(number_format($a['mk'], 2, ',', '.')); ?></div>
+                                <div class="d-flex align-items-center py-2 pr-3 w-100"><?= Simpl(number_format($a['mk'], 2, ',', '.')); ?></div>
                             </td>
                             <!-- <td class="fw-bold"><?= ($a['tt'] == 1) ? "<span style='color:red;'>Buy<span>" : "<span style='color:green;'>Sell<span>"; ?></td> -->
                             <!-- <td><?= $a['st']; ?></td> -->
